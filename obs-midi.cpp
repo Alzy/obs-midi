19f1ce03220e767fc746e2a0e868a3f9f36882dc
Colin Edwards <colin@recursivepenguin.com>
2017-03-08 00:01:22 -0600

    Soms proof of concept commands

--- obs-midi.cpp
+++ obs-midi.cpp
@@ -18,14 +18,37 @@ void rtmidicallback( double deltatime, std::vector< unsigned char > *message, vo
 					blog(LOG_INFO, "Channel %d Note %02X off", channel, message->at(1));
 					break;
 				case 0x9: // Note on
+				{
 					blog(LOG_INFO, "Channel %d Note %02X on", channel, message->at(1));
+
+					obs_source_t *source = obs_get_source_by_name("Scene 2");
+
+					obs_frontend_set_current_scene(source);
+
+					obs_source_release(source);
+
 					break;
+				}
 				case 0xA: // Key pressure
 
 					break;
 				case 0xB: // Control Change
+				{
+
 					blog(LOG_INFO, "Channel %d Control %02X updated %02X", channel, message->at(1), message->at(2));
+
+					float amount = message->at(2);
+
+					amount = amount/127.0f;
+
+					obs_source_t *source = obs_get_source_by_name("Mic/Aux");
+
+					obs_source_set_volume(source, amount);
+
+					obs_source_release(source);
+
 					break;
+			}
 				case 0xC: // Program Change
 
 					break;
@@ -43,8 +66,56 @@ void rtmidicallback( double deltatime, std::vector< unsigned char > *message, vo
 	}
 }
 
+void test_set_colors() {
+	std::vector<unsigned char> message;
+
+	RtMidiOut *midiout = 0;
+	// RtMidiOut constructor
+	try {
+		midiout = new RtMidiOut();
+	}
+	catch (RtMidiError &error) {
+		// Handle the exception here
+		error.printMessage();
+	}
+
+	// Check outputs.
+	std::string portName;
+	unsigned int nPorts = midiout->getPortCount();
+	std::cout << "\nThere are " << nPorts << " MIDI output ports available.\n";
+	for ( unsigned int i=0; i<nPorts; i++ ) {
+		try {
+			portName = midiout->getPortName(i);
+		}
+		catch (RtMidiError &error) {
+			error.printMessage();
+		}
+		std::cout << "  Output Port #" << i+1 << ": " << portName << '\n';
+	}
+	std::cout << '\n';
+
+	midiout->openPort( 0 );
+
+	message.push_back( 90 );
+	message.push_back( 0 );
+	message.push_back( 1 );
+	midiout->sendMessage( &message );
+
+	message[0] = 90;
+	message[1] = 1;
+	message[2] = 2;
+	midiout->sendMessage( &message );
+}
+
+static void OBSEvent(enum obs_frontend_event event, void *)
+{
+
+}
+
 bool obs_module_load(void)
 {
+	test_set_colors();
+
 	RtMidiIn *midiin = 0;
 	// RtMidiIn constructor
 	try {
@@ -60,6 +131,8 @@ bool obs_module_load(void)
 	midiin->openPort(0, "");
 
 	midiin->setCallback(&rtmidicallback, 0);
+
+	obs_frontend_add_event_callback(OBSEvent, nullptr);
 
 	return true;
 }

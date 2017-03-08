3e344bbb83d97b311f1a2e7497ea7e32ff881843
Colin Edwards <colin@recursivepenguin.com>
2017-03-07 19:27:29 -0600

    Some cleanup + windows deps

--- obs-midi.cpp
+++ obs-midi.cpp
@@ -6,12 +6,6 @@ OBS_DECLARE_MODULE()
 
 void rtmidicallback( double deltatime, std::vector< unsigned char > *message, void *userData )
 {
-	/*unsigned int nBytes = message->size();
-	for ( unsigned int i=0; i<nBytes; i++ )
-		std::cout << "Byte " << i << " = " << (int)message->at(i) << ", ";
-	if ( nBytes > 0 )
-		std::cout << "stamp = " << deltatime << std::endl;*/
-
 	unsigned int nBytes = message->size();
 
 	if (nBytes > 0) {

0370f89aa7f985c69c43cf41aa0e5fd40ae20bfe
Alzy <gonzalezalfredo101@gmail.com>
2020-05-09 15:11:41 -0700

    init midi agent`

---
+++ midi-agent.cpp
@@ -0,0 +1,43 @@
+/*
+obs-midi
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/>
+*/
+
+#include <obs-frontend-api.h>
+#include <QtCore/QTime>
+
+#include "utils.h"
+#include "midi-agent.h"
+
+
+MidiAgent::MidiAgent()
+{
+	//
+}
+
+MidiAgent::~MidiAgent()
+{
+	//
+}
+
+void MidiAgent::SetVolume(QString source, float volume)
+{
+	OBSSourceAutoRelease obsSource = obs_get_source_by_name(source.toUtf8());
+	if (!obsSource) {
+		return; // source does not exist
+	}
+
+	obs_source_set_volume(obsSource, volume);
+}

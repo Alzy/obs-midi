4487c27b196bdfd698dfd89a2b893ac6ca3fb498
Alzy <gonzalezalfredo101@gmail.com>
2020-05-08 17:36:55 -0700

    implemented outputtin midi messages through callback

--- obs-midi.cpp
+++ obs-midi.cpp
@@ -5,6 +5,19 @@
 
 OBS_DECLARE_MODULE()
 
+
+void midiin_callback(double deltatime, std::vector<unsigned char> *message, void *userData)
+{
+	unsigned int nBytes = message->size();
+	for (unsigned int i = 0; i < nBytes; i++) {
+		blog(LOG_INFO, "Byte %d = %d", i, (int)message->at(i));
+	}
+	if (nBytes > 0)
+		std::cout << "stamp = " << deltatime << std::endl;
+}
+
+
+
 bool obs_module_load(void)
 {
 	blog(LOG_INFO, "MIDI LOADED ");
@@ -22,10 +35,14 @@ bool obs_module_load(void)
 	for (unsigned int i = 0; i < nPorts; i++) {
 		try {
 			std::string portName = midiin->getPortName(i);
-			blog(LOG_INFO, "MIDI DEVICE FOUND: %s", portName);
+			blog(LOG_INFO, "MIDI DEVICE FOUND: %s", &portName);
 		} catch (RtMidiError &error) {
 			error.printMessage();
 		}
 	}
+
+	midiin->openPort(0);
+	midiin->setCallback(&midiin_callback);
+
 	return true;
 }

a70b1b6fa982aa0fbc50f5be4c4793ddd8c1c23b
Christopher P Yarger <cpyarger@gmail.com>
2020-06-21 20:56:30 -0400

    windows

--- CI/package-windows.cmd
+++ CI/package-windows.cmd
@@ -1,11 +1,15 @@
 mkdir package
 cd package
-dir D:\a\1\s\
+
+
 git rev-parse --short HEAD > package-version.txt
 set /p PackageVersion=<package-version.txt
 del package-version.txt
 
 REM Package ZIP archive
+dir ..\
+dir .
+
 7z a "obs-midi-%PackageVersion%-Windows.zip" "..\release\*"
 
 REM Build installer

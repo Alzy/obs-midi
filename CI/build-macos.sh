cde751f12dfdc30f5f408ccb5c4db1754fe37c6f
Christopher P Yarger <cpyarger@gmail.com>
2020-06-23 03:13:41 -0400

    fix2?

--- CI/build-macos.sh
+++ CI/build-macos.sh
@@ -19,6 +19,7 @@ fi
 echo "[obs-midi] Building 'obs-midi' for macOS."
 mkdir -p build && cd build
 cmake .. \
+  -DisAzure=true\ 
 	-DQTDIR=/usr/local/opt/qt \
 	-DLIBOBS_INCLUDE_DIR=../../obs-studio/libobs \
 	-DLIBOBS_LIB=../../obs-studio/libobs \

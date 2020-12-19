f9b39440a07c03e397697e3333acdc8727d00c38
Alzy <gonzalezalfredo101@gmail.com>
2020-12-19 00:23:05 -0800

    Azure pipelines test + fix

--- src/obs-midi.cpp
+++ src/obs-midi.cpp
@@ -45,7 +45,7 @@ eventsPtr _eventsSystem;
 
 bool obs_module_load(void)
 {
-	blog(LOG_INFO, "MIDI LOADED :)");
+	blog(LOG_INFO, "MIDI LOADED! :)");
 
 	// Device Manager Setup
 	_deviceManager = DeviceManagerPtr(new DeviceManager());

07e1a342e2b54e28149ddbf9977f3b9e0238277c
Christopher P. Yarger <cpyarger@gmail.com>
2021-01-23 04:29:57 -0500

    Update events.cpp

--- src/events.cpp
+++ src/events.cpp
@@ -1171,7 +1171,6 @@ void events::OnSourceVolumeChange(void *param, calldata_t *data)
 	obs_data_set_string(fields, "sourceName", obs_source_get_name(source));
 	obs_data_set_double(fields, "volume", volume);
 	self->broadcastUpdate("SourceVolumeChanged", fields);
-	obs_source_release(source);
 }
 
 /**

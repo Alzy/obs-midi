5ea214b7d95005532986cff54a75de033f3741c0
Christopher P Yarger <cpyarger@gmail.com>
2021-02-06 20:02:19 -0500

    ubuntu deps fix 2

--- CI/install-dependencies-ubuntu.sh
+++ CI/install-dependencies-ubuntu.sh
@@ -3,7 +3,7 @@ set -ex
 
 sudo add-apt-repository -y ppa:obsproject/obs-studio
 sudo apt-get -qq update
-echo updated
+
 sudo cp /etc/apt/sources.list /etc/apt/sources.list~
 sudo sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
 sudo apt-get update

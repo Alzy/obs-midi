5315e58dc4988b78313405811d10d21f31e9e689
Christopher P Yarger <cpyarger@gmail.com>
2021-02-12 14:02:41 -0500

    yay

--- CI/check-format.sh
+++ CI/check-format.sh
@@ -6,8 +6,11 @@ if [[ $dirty ]]; then
 	echo "================================="
     echo "Files were not formatted properly"
     echo "$dirty"
-    echo "================================="
-    git add -A
+
+		echo "================================="
+		git config --global user.email "cpyarger@gmail.com"
+		git config --global user.name "Christopher P Yarger"
+		git add -A
 		git commit -am "Clang formatted Files"
 		git push
 fi

172ce1ab169b8ef57c1f330ff0e447b7a7b71f76
Christopher P. Yarger <cpyarger@gmail.com>
2021-03-05 06:11:32 -0500

    clang-format

--- src/obs-controller.cpp
+++ src/obs-controller.cpp
@@ -209,20 +209,20 @@ void OBSController::DisablePreview()
 		[](void *param) {
 			if (obs_frontend_preview_enabled()) {
 				obs_frontend_set_preview_enabled(false);
-            }
-        (void)param;
-        
-    },
+			}
+			(void)param;
+		},
 		nullptr, true);
-    
 }
 void OBSController::EnablePreview()
 {
 	obs_queue_task(
-                   OBS_TASK_UI, [](void *param) {
-        obs_frontend_set_preview_enabled(true);
-        (void)param;
-     }, nullptr, true);
+		OBS_TASK_UI,
+		[](void *param) {
+			obs_frontend_set_preview_enabled(true);
+			(void)param;
+		},
+		nullptr, true);
 }
 /**
  * Change the active scene collection.
@@ -263,12 +263,12 @@ void OBSController::TransitionToProgram()
 	int currentDuration = obs_frontend_get_transition_duration();
 	obs_source_t *transition = obs_frontend_get_current_transition();
 	/**
-	* If Transition from hook is not Current Transition, and if it is not an empty Value, then set current transition
-	*/
+	 * If Transition from hook is not Current Transition, and if it is not an empty Value, then set current transition
+	 */
 	if ((hook->transition != "Current Transition")) {
 		Utils::SetTransitionByName(hook->transition);
 	}
-	if (hook->int_override){
+	if (hook->int_override) {
 		obs_frontend_set_transition_duration(*hook->int_override);
 	}
 	obs_frontend_preview_program_trigger_transition();
@@ -567,17 +567,17 @@ void OBSController::play_pause_media_source()
 	case obs_media_state::OBS_MEDIA_STATE_ENDED:
 		obs_source_media_restart(source);
 		break;
-        case OBS_MEDIA_STATE_NONE:
-            break;
-        case OBS_MEDIA_STATE_OPENING:
-            break;
-        case OBS_MEDIA_STATE_BUFFERING:
-            break;
-        case OBS_MEDIA_STATE_STOPPED:
-            break;
-        case OBS_MEDIA_STATE_ERROR:
-            break;
-    }
+	case OBS_MEDIA_STATE_NONE:
+		break;
+	case OBS_MEDIA_STATE_OPENING:
+		break;
+	case OBS_MEDIA_STATE_BUFFERING:
+		break;
+	case OBS_MEDIA_STATE_STOPPED:
+		break;
+	case OBS_MEDIA_STATE_ERROR:
+		break;
+	}
 }
 // TODO:: Fix this
 void OBSController::toggle_studio_mode()
